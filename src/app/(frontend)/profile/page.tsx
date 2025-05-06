d8863778c52f2cff79946ccc89cb1811ef71e21c
eeee3e <modhoop.mitra@mail.utoronto.ca>
2025-05-05 20:16:49 -0400

    added profile page template

---
+++ src/app/(frontend)/profile/page.tsx
@@ -0,0 +1,12 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Profile from '@/components/Profile'
+
+const Page = () => {
+  return (
+    <MaxWidthWrapper>
+      <Profile></Profile>
+    </MaxWidthWrapper>
+  )
+}
+
+export default Page
