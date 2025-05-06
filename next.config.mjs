6c644c25317c715c8ef974f1e5c6793cd8af56d6
John Kim <jonbkim7@gmail.com>
2025-05-06 13:37:57 -0400

    chore: added permission URL to images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,19 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: 'jarvis-profile-viewer.vercel.app',
+      },
+    ],
+    unoptimized: true,
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
