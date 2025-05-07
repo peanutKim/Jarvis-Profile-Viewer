117d8c20f17e976c0ac3472c745105fe25d1aac4
Ilyas-Ismail <90821178+Ilyas-Ismail@users.noreply.github.com>
2025-05-07 11:40:33 -0400

    Update page.tsx

--- src/app/(frontend)/consultants/page.tsx
+++ src/app/(frontend)/consultants/page.tsx
@@ -14,53 +14,54 @@ import { Suspense } from 'react';
 const ConsultantsPageContent = () => {
   const searchParams = useSearchParams();
   const [consultants, setConsultants] = useState<Consultant[]>([]);
+  const [masterCopy, setMasterCopy] = useState<Consultant[]>([]);
   const [filter, setFilter] = useState("");
 
+  const fetchConsultants = async () => {
+    const response = await fetch('/api/get-consultants');
+    const data = await response.json();
+    setMasterCopy(data.docs);
+
+    if (searchParams.toString() === "") {
+      return setConsultants(data.docs);
+    }
+
+    if (searchParams.get("email") != null) {
+      data.docs = data.docs.filter((item: { email: (string | null)[] }) => item.email.includes(searchParams.get("email")));
+    }
+
+    if (searchParams.get("firstName") != null) {
+      const firstName = String(searchParams.get("firstName")).toUpperCase();
+      data.docs = data.docs.filter((item: { firstName: string }) => item.firstName.toUpperCase().includes(firstName));
+    }
+
+    if (searchParams.get("lastName") != null) {
+      const lastName = String(searchParams.get("lastName")).toUpperCase();
+      data.docs = data.docs.filter((item: { lastName: string }) => item.lastName.toUpperCase().includes(lastName));
+    }
+
+    if (searchParams.get("phoneNumber") != null) {
+      data.docs = data.docs.filter((item: { phoneNumber: (string | null)[] }) => item.phoneNumber.includes(searchParams.get("phoneNumber")));
+    }
+
+    if (searchParams.get("bio") != null) {
+      const bio = String(searchParams.get("bio")).toUpperCase();
+      data.docs = data.docs.filter((item: { bio: string }) => item.bio.toUpperCase().includes(bio));
+    }
+
+    setConsultants(data.docs);
+  };
+
   useEffect(() => {
-    const fetchConsultants = async () => {
-      const response = await fetch('/api/get-consultants');
-      const data = await response.json();
-
-      if (searchParams.toString() === "") {
-        return setConsultants(data.docs);
-      }
-
-      if (searchParams.get("email") != null) {
-        data.docs = data.docs.filter((item: { email: (string | null)[] }) => item.email.includes(searchParams.get("email")));
-      }
-
-      if (searchParams.get("firstName") != null) {
-        const firstName = String(searchParams.get("firstName")).toUpperCase();
-        data.docs = data.docs.filter((item: { firstName: string }) => item.firstName.toUpperCase().includes(firstName));
-      }
-
-      if (searchParams.get("lastName") != null) {
-        const lastName = String(searchParams.get("lastName")).toUpperCase();
-        data.docs = data.docs.filter((item: { lastName: string }) => item.lastName.toUpperCase().includes(lastName));
-      }
-
-      if (searchParams.get("phoneNumber") != null) {
-        data.docs = data.docs.filter((item: { phoneNumber: (string | null)[] }) => item.phoneNumber.includes(searchParams.get("phoneNumber")));
-      }
-
-      if (searchParams.get("bio") != null) {
-        const bio = String(searchParams.get("bio")).toUpperCase();
-        data.docs = data.docs.filter((item: { bio: string }) => item.bio.toUpperCase().includes(bio));
-      }
-
-      setConsultants(data.docs);
-    };
-
     fetchConsultants();
   }, [searchParams]);
 
-  function submitSearch() {
-    if (filter !== "") {
-      window.location.replace(window.location.origin + "/consultants?firstName=" + filter);
-    } else {
-      window.location.replace(window.location.origin + "/consultants");
+  useEffect(() => {
+    if (filter != null) {
+      const firstName = filter.toUpperCase();
+       setConsultants(masterCopy.filter((item: { firstName: string }) => item.firstName.toUpperCase().includes(firstName)))
     }
-  }
+  }, [filter])
 
   return (
     <div>
@@ -75,9 +76,6 @@ const ConsultantsPageContent = () => {
               setFilter((e.target as HTMLInputElement).value);
             }}
           />
-          <Button type="submit" onClick={submitSearch}>
-            Search
-          </Button>
         </div>
       </div>
       <div className="mb-5 mt-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
